94320ae7936cb83004a2aa61502b872f4ff1cef7
Ayrton Gomes <ayrton.gomes@sintaxy.com>
2025-10-29 11:36:47 -0100

    implement options on embeed

--- tools/server/server.cpp
+++ tools/server/server.cpp
@@ -4554,7 +4554,7 @@ std::pair<m8p::M8_Error, m8p::M8_Obj*> LLM_EMBED(
                 for (auto &res : results) {
                     GGML_ASSERT(dynamic_cast<server_task_result_embd*>(res.get()) != nullptr);
                     json R = res->to_json();
-                    std::vector<float> &vec = json_value(R, "embedding", json::array()).get<std::vector<float>>();
+                    std::vector<float> vec = json_value(R, "embedding", json::array()).get<std::vector<float>>();
                     for (std::vector<float>::iterator i=vec.begin(); i!=vec.end(); ++i) {
                         REG[rdest]->AR_F32.push_back((float)*i);
                     }
