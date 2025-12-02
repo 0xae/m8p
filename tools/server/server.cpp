02bbecaa9b3f54c8215ca6cfced164ae6b4b196f
Ayrton Gomes <com.ayrton@gmail.com>
2025-12-02 04:39:11 -0100

    implement stream_sink

--- tools/server/server.cpp
+++ tools/server/server.cpp
@@ -5457,7 +5457,8 @@ std::pair<m8p::M8_Error, m8p::M8_Obj*> STREAM_SINK(
 
         auto sink = GlobalSession[M8->Name].sink;
         std::string output = "data: " + rsource + "\n\n";
-        sink->write(output.data(), output.size());
+        // sink->write(output.data(), output.size());
+        sink->os << output << std::flush;
         // auto was_sent = server_sent_event(*sink, json{{"event", rsource}});
         // sink->done();
         // std::string nl = "\n";
