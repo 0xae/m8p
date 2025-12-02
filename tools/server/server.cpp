dab381098d4ca2f45e53d673ac4b9bc8ad73685e
Ayrton Gomes <com.ayrton@gmail.com>
2025-12-02 04:31:13 -0100

    implement stream_sink

--- tools/server/server.cpp
+++ tools/server/server.cpp
@@ -7092,7 +7092,7 @@ std::string M8_BANNER =
                 // will handle all custom instr
                 m8p::RegisterVirtual(m8, "__all__", virtualvm);
 
-                sink.done();
+                // sink.done();
 
                 std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
                 std::pair<m8p::M8_Error, m8p::M8_Obj*> Ret = m8p::Run(m8, code_buf);
