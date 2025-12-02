b9d1d9ad096c267fdf97b06cb063046196b02690
Ayrton Gomes <com.ayrton@gmail.com>
2025-12-02 08:38:26 -0100

    try writing sink inside another clause

--- tools/server/server.cpp
+++ tools/server/server.cpp
@@ -5466,7 +5466,7 @@ std::pair<m8p::M8_Error, m8p::M8_Obj*> STREAM_SINK(
         auto sink = GlobalSession[M8->Name].sink;
         auto result_handle = [sink, &rsource]() {
             server_sent_event(*sink, json{{"event", rsource}});
-        } 
+        };
 
         ctx_server->sink_stream(result_handle);
         // std::string output = "data: " + rsource + "\n\n";
