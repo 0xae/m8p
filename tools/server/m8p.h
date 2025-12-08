190e7a30673589eb37d9f999b673ae7e1c3be3f9
Ayrton Gomes <com.ayrton@gmail.com>
2025-12-08 04:58:28 -0100

    implement protocol newline handling

--- tools/server/m8p.h
+++ tools/server/m8p.h
@@ -615,7 +615,7 @@ namespace m8p {
         return v>0? v : -v;
     }
 
-    void replaceAll(std::string& str, std::string &from, std::string &to) {
+    void replaceAll(std::string& str, const std::string &from, const std::string &to) {
         size_t start_pos = 0;
         while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
             str.replace(start_pos, from.length(), to);
--- tools/server/server.cpp
+++ tools/server/server.cpp
@@ -693,7 +693,7 @@ struct server_task_result {
     virtual ~server_task_result() = default;
 };
 
-void replaceAll2(std::string& str, std::string &from, std::string &to) {
+void replaceAll2(std::string& str, const std::string &from, const std::string &to) {
     size_t start_pos = 0;
     while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
         str.replace(start_pos, from.length(), to);
