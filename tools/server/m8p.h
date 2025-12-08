03fd18f3a7423dd4a0e4548edb3df9bc4e6f92a0
Ayrton Gomes <com.ayrton@gmail.com>
2025-12-08 04:52:55 -0100

    implement protocol newline handling

--- tools/server/m8p.h
+++ tools/server/m8p.h
@@ -228,7 +228,7 @@ namespace m8p {
     int InitMP8(M8System *m8);
     void DestroyMP8(M8System *m8);
     std::vector<std::string> __split(std::string s, std::string delimiter);
-    void replaceAll(std::string& str, std::string& from, std::string& to);
+    void replaceAll(std::string& str, const std::string& from, const std::string& to);
     std::pair<M8_Error, M8_Obj*> NO_OP(M8System* m8, std::vector<std::string> params);
     std::pair<M8_Error, M8_Obj*> Store_OP(M8System* M8, std::vector<std::string> params);
     std::pair<M8_Error, M8_Obj*> Clear_OP(M8System* M8, std::vector<std::string> params);
