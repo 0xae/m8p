fcf9cef3dc6ed5fdd00ab2bc6aa126c3a4ee56bd
Ayrton Gomes <com.ayrton@gmail.com>
2025-12-10 00:24:35 -0100

    working on prompt engineering

--- tools/server/server.cpp
+++ tools/server/server.cpp
@@ -5702,7 +5702,8 @@ std::pair<m8p::M8_Error, m8p::M8_Obj*> LLM_MULTI_TURN(
                     {{"role", "system"}, {"content", system_prompt}},
                     {{"role", "user"}, {"content", last_question}},
                     {{"role", "assistant"}, {"content", last_answer}},
-                    {{"role", "user"},   {"content", userReply}}
+                    {{"role", "user"}, {"content", userReply}},
+                    {{"role", "assistant"},  {"content", "Answer:"}}
                 };
             }
 
