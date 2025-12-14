5af63fbe1da16fe213dfc5fca7ab4669138d6a37
Ayrton Gomes <com.ayrton@gmail.com>
2025-12-14 16:15:29 -0100

    improve message chat

--- tools/server/server.cpp
+++ tools/server/server.cpp
@@ -5738,7 +5738,7 @@ std::pair<m8p::M8_Error, m8p::M8_Obj*> LLM_OPENAI2(
             << "<end_of_turn>\n"
             << "<start_of_turn>user\n"
             << prompt
-            << "<end_of_turn>\n"
+            << "<end_of_turn>\n";
             // << "<start_of_turn>assistant\n";
 
         chat_prompt = ss_prompt.str();
