cd7e7160db6bf4ea4da978bab2d11de8851bef23
Ayrton Gomes <com.ayrton@gmail.com>
2025-12-14 16:04:47 -0100

    improve message chat

--- tools/server/server.cpp
+++ tools/server/server.cpp
@@ -5731,9 +5731,19 @@ std::pair<m8p::M8_Error, m8p::M8_Obj*> LLM_OPENAI2(
             files
         );
 
-        if (chat_prompt!="") {
-            data["prompt"] = chat_prompt;
-        }
+        std::stringstream ss_prompt;
+        ss_prompt 
+            << "<start_of_turn>system\n"
+            << system_prompt 
+            << "<end_of_turn>\n"
+            << "<start_of_turn>user\n"
+            << prompt
+            << "<end_of_turn>\n"
+            << "<start_of_turn>assistant\n";
+
+        chat_prompt = ss_prompt.str();
+
+        data["prompt"] = chat_prompt;
 
         std::cout << "=======> llm_openai2[]: "
             << data.dump() 
