111eae84e357640a55ef87221329213fc9cf7e20
Ayrton Gomes <com.ayrton@gmail.com>
2025-12-16 13:46:07 -0100

    implement projection

--- tools/server/storage-engine/TensorGraph.hpp
+++ tools/server/storage-engine/TensorGraph.hpp
@@ -866,13 +866,7 @@ public:
                                 } else {
                                     continue;
                                 }
-                                std::string COMMAND =  "GET("+
-                                    args[0]+", "+
-                                    args[1]+", "+
-                                    args[2]+", "+
-                                    id+", "+
-                                    typeL+
-                                ")";
+                                std::string COMMAND =  "GET("+args[0]+", "+args[1]+", "+args[2]+", "+id+", "+typeL+")";
                                 std::string result__x = Execute(db, COMMAND);
                                 result_ss  << result__x << "\t";                     
                             }
